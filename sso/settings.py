092f7f01e79ce54737a1b23bebcca0d074aec603
John <johnnkonge2020@gmail.com>
2024-03-24 19:22:24 +0300

    made migrations

--- sso/settings.py
+++ sso/settings.py
@@ -67,6 +67,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #all auth middleware
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'sso.urls'
